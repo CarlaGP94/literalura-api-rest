98f9bf7cee3562f638774cdd17a793fd5b82d796
carlapasadi@gmail.com <carlapasandi@hotmail.com>
2025-06-27 04:58:49 -0300

    Inicio de estructura

---
+++ src/main/java/com/alura/literalura_api_rest/LiteraluraApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.alura.literalura_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/literalura_api_rest/LiteraluraApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.literalura_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
